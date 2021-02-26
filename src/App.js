f79caf8940eae457e36302c0dd5df2eef974cb41
DonquixoteSabo <mixboksu@gmail.com>
2021-02-26 18:54:27 +0100

    create product component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-// I want to test creating new branch in git :D
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-          <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right is reserved.
-      </footer>
-    </div>
-  );
+import React, { Component } from 'react';
+
+import data from './data.json';
+import Products from './components/Products';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+            <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+                  <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+               Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All right is reserved.
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
