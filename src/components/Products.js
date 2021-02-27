f010b8973fb649052b026275529faedf031ed648
DonquixoteSabo <mixboksu@gmail.com>
2021-02-27 07:09:27 +0100

    append cart-component

--- src/components/Products.js
+++ src/components/Products.js
@@ -20,7 +20,7 @@ class Products extends Component {
                                     <div>
                                         {formatCurrency(product.price)}
                                     </div>
-                                    <button className='button primary'>
+                                    <button className='button primary' onClick={() => this.props.addToCart(product)}>
                                         Add to Cart
                                     </button>
                                 </div>
